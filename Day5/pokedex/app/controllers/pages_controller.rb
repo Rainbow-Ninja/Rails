af876e9045dd0608f2d158c7daa45cd2d2d7c6d1
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2019-06-18 15:26:03 +1000

    views forms rails

--- Day5/pokedex/app/controllers/pages_controller.rb
+++ Day5/pokedex/app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     end
 
     def create
-        url = "https://pokeapi.co/api/v2/pokemon/"
+        url = "https://pokeapi.co/api/v2/pokemon/#{pokemon-name}" #FIX THIS!!!!
         gem_plus_url = RestClient.get(url)
         @results = JSON.parse(gem_plus_url)
     end
---
+++ Day6/crud-lesson/app/controllers/movies_controller.rb
@@ -0,0 +1,46 @@
+class MoviesController < ApplicationController
+
+    #show all movies
+    def index
+        @movies = session[:movies]
+    end
+
+    #post a new movie
+    def create
+        # session is a rails way of storing data temporarily in a cookie
+        if session[:movies] == nil
+            session[:movies] = []
+        end
+
+        session[:movies].push(params[:movie])
+        puts "session movie data"
+        puts session[:movies]
+
+        redirect_to movie_path(session[:movies].length - 1) #pass the last movie we just added to the show page and redirect there
+        # puts "==================="
+        # puts params[:movie]
+        # puts "==================="
+        # @movies = []
+        # @movies.push(params[:movie])
+    end
+
+    #add a new movie
+    def new
+   
+    end
+
+    #show a single movie
+    def show
+        @movie = session[:movies][params[:id].to_i]
+    end
+
+    def edit
+        @movie = session[:movies][params[:id].to_i]
+    end
+
+    def update
+        puts "Update Method is Executed -----------------"
+        session[:movies][params[:id].to_i] = params[:movie]
+        redirect_to movie_path(params[:id].to_i)
+    end
+end
