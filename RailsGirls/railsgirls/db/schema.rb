e9e0e4f5d2b2afce092d62c28bda83b73ac22f54
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2019-10-02 10:40:18 +1000

    railsgirls work

--- RailsGirls/railsgirls/db/schema.rb
+++ RailsGirls/railsgirls/db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_011122) do
+ActiveRecord::Schema.define(version: 2019_09_26_025745) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.string "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
